52dc51a1a4ff010effe8ad2b6e7858daed6492d9
Faqih Madani <faqihfaqih22@gmail.com>
2024-01-19 15:22:54 +0700

    register feature

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,8 +1,9 @@
-import { useState } from 'react'
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import { Toaster } from 'react-hot-toast';
+
 import Home from './pages/Home';
 import Register from './pages/Register';
 import Login from './pages/Login';
@@ -25,6 +26,31 @@ function App() {
   return (
     <>
       <RouterProvider router={router}></RouterProvider>
+      <Toaster
+        position="top-center"
+        reverseOrder={false}
+        gutter={8}
+        containerClassName=""
+        containerStyle={{}}
+        toastOptions={{
+          // Define default options
+          className: '',
+          duration: 5000,
+          style: {
+            background: '#363636',
+            color: '#fff',
+          },
+
+          // Default options for specific types
+          success: {
+            duration: 3000,
+            theme: {
+              primary: 'green',
+              secondary: 'black',
+            },
+          },
+        }}
+      />
     </>
   )
 }
--- front-end/src/pages/Register.jsx
+++ front-end/src/pages/Register.jsx
@@ -1,21 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import axios from 'axios'
+import toast from 'react-hot-toast'
 
 function Register() {
+    const [data, setData] = useState({
+        name: "",
+        email: "",
+        password: "",
+        password_confirmation: ""
+    })
+
+    const handleChange = (e) => {
+        setData((prev) => {
+            return { ...prev, [e.target.name]: e.target.value }
+        })
+
+        console.log(data);
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+        try {
+            const res = await axios.post(`${import.meta.env.VITE_BASE_URL}/auth/register`, data)
+
+            console.log(res.data);
+        } catch (error) {
+            console.log(error);
+            toast.error("Error")
+        }
+
+    }
+
+
     return (
         <div className='h-screen font-serif w-screen flex items-center justify-end bg-gradient-to-br from-purple-950 to-pink-600'>
             <div className='bg-slate-50 h-full px-32 flex items-center justify-center'>
                 <div className='md:w-3/4flex items-center justify-center flex-col'>
                     <h1 className='text-2xl font-bold text-slate-900 text-center'>Register new account</h1>
                     <form className='mt-7 w-full gap-y-3 flex flex-col relative'>
-                        <input type="text" placeholder='Username' className='p-2 border-[2px] border-purple-800' />
-                        <input type="email" placeholder='Email' className='p-2 border-[2px] border-purple-800' />
-                        <input type="password" placeholder='Password' className='p-2 border-[2px] border-purple-800' />
-                        <input type="password" placeholder='Confirm Password' className='p-2 border-[2px] border-purple-800' />
+                        <input onChange={handleChange} name='name' type="text" placeholder='Username' className='p-2 border-[2px] border-purple-800' />
+                        <input onChange={handleChange} name='email' type="email" placeholder='Email' className='p-2 border-[2px] border-purple-800' />
+                        <input onChange={handleChange} name='password' type="password" placeholder='Password' className='p-2 border-[2px] border-purple-800' />
+                        <input onChange={handleChange} name='password_confirmation' type="password" placeholder='Confirm Password' className='p-2 border-[2px] border-purple-800' />
 
                         <p className='text-red-600 text-sm absolute bottom-32'>Error message</p>
 
-                        <button type='submit' className='bg-purple-950 text-slate-50 font-semibold p-3 mt-10'>Register</button>
+                        <button onClick={handleSubmit} type='submit' className='bg-purple-950 text-slate-50 font-semibold p-3 mt-10'>Register</button>
                         <p className='text-center mt-5'>Already have an account?
                             <Link to={'/login'} className='ml-1 hover:underline text-purple-950'>
                                 Login
